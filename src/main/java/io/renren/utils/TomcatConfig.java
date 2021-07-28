787c0c0a7afe7a55decbc4280b968b6b9e499898
zhengmingming9 <zhengmingming9@jd.com>
2021-07-28 14:42:58 +0800

    代码生成器

---
+++ src/main/java/io/renren/utils/TomcatConfig.java
@@ -0,0 +1,19 @@
+package io.renren.utils;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public TomcatServletWebServerFactory tomcatServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+        });
+        return tomcatServletWebServerFactory;
+    }
+}
