c9c2145b95a4dff4c455a751d652adb1c7d47173
zhengmingming9 <zhengmingming9@jd.com>
2021-09-03 17:03:32 +0800

    代码生成器

--- src/main/resources/template/generator-mybatis/entity/${className}.java
+++ src/main/resources/template/generator-mybatis/entity/${className}.java
@@ -2,11 +2,13 @@ package com.jd.ept.walmart.domain.entity;
 
 import java.util.Date;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
 
 #if(${hasBigDecimal})
 import java.math.BigDecimal;
@@ -15,7 +17,7 @@ import java.math.BigDecimal;
 
 /**
  * ${comments}
- * 
+ *
  * @author ${author}
  * @email ${email}
  * @date ${datetime}
@@ -23,18 +25,24 @@ import java.math.BigDecimal;
 @Entity
 @Table(name="${tableName}")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ${className} {
 
-#foreach ($column in $columns)
+		#foreach ($column in $columns)
 		#if($column.columnName == $pk.columnName)
-	@Id
-	private $column.attrType $column.attrname;
+@Id
+private $column.attrType $column.attrname;
 		#end
 		#if($column.columnName != $pk.columnName)
-	@Column(name="${column.columnName}")
-	private $column.attrType $column.attrname;
+/**
+ *$column.comments
+ */
+@Column(name="${column.columnName}")
+private $column.attrType $column.attrname;
 		#end
-#end
+		#end
 
 
-}
+		}
